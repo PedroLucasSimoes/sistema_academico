62410a13cd88d08a027729c020ec0796b84b6d73
Pedro Lucas Simões Cabral <lucaspb3012@gmail.com>
2021-07-31 15:39:07 -0300

    Primeiro commit

---
+++ codigo2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$nome = $_POST["nome"];
+$cpf = $_POST["cpf"];
+$endereço = $_POST["endereço"];
+$cep = $_POST["cep"];
+$telefone = $_POST["telefone"];
+
+$senha = $_POST["senha"];
+echo $senha;
+$postsenha = base64_encode("$senha");
+echo $senha;
+
+$conn = mysqli_connect("localhost", "root", "123");
+if (!$conn) {
+die("Não foi possível conectar ao banco de dados.
+        Erro detectado:" . mysqli_error());
+}
+
+mysqli_set_charset($conn, "utf8");
+
+echo "Conectado com sucesso";
+
+mysqli_select_db($conn, "sis_academico");
+
+$sql = " INSERT INTO alunos
+(id, nome, cpf, endereço, cep, telefone, senha)
+VALUES ('0', '$nome', '$cpf', '$endereço', '$cep', '$telefone', '$postsenha' ) ";
+
+if (mysqli_query($conn ,$sql)) {
+    echo "New record created";
+} else {
+    echo "Error". $sql. "<br>".mysqli_error($conn);
+}
+
+mysqli_close($conn)
+
+?>
+</body>
+</html>
---
+++ connect.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+$connection = mysqli_connect("localhost", "root", "123");
+if (!$connection) {
+die("Não foi possível conectar ao banco de dados.
+        Erro detectado:" . mysqli_error());
+}
+
+?>
+</body>
+</html>
